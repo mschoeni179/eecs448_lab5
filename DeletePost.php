a3afadaa07fd6147428d80d0ebc34b375c28458d
Malena Schoeni <mschoeni789@gmail.com>
2019-10-31 17:10:39 -0500

    add DeletePost capability

---
+++ DeletePost.php
@@ -0,0 +1,10 @@
+<?php
+$myPass = "seishae4";
+$mysqli = new mysqli("mysql.eecs.ku.edu", "msch179", $myPass, "malena");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+  ?>
