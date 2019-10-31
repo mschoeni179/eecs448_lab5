47a8558bee85dc05c79dc899c69a440f953db0ed
Malena Schoeni <mschoeni789@gmail.com>
2019-10-31 15:27:39 -0500

    feat: add view user pots capabilties and pages

---
+++ ViewUserPosts.php
@@ -0,0 +1,26 @@
+<?php
+    $myPass = "seishae4";
+    $mysqli = new mysqli("mysql.eecs.ku.edu", "msch179", $myPass, "malena");
+
+    if ($mysqli->connect_errno) {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
+
+    $data = "SELECT userID FROM Users";
+    $result = $mysqli->query($data);
+    echo "<table>";
+    if ($result->num_rows >0)
+    {
+        while($row = mysqli_fetch_assoc($result))
+        {
+            echo "<option>" . $row["userID"] . "</option>";
+        }
+        echo "<input type='submit' name='submit' value='Submit'>";
+        echo "</select> ";    
+    }
+    else
+    {
+        echo "Error, user not found\n";
+    }
+?>
