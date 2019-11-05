c91b2dd773f18bdc1e7e5b3308c11dd7357df8f4
Malena Schoeni <mschoeni789@gmail.com>
2019-11-04 23:31:02 -0600

    add deletePost

--- DeletePost.php
+++ DeletePost.php
@@ -8,8 +8,26 @@ if ($mysqli->connect_errno) {
     exit();
 }
 
-
-
+$delete = 1;
+$query = "SELECT post_id FROM Posts ORDER BY post_id";
+$totalPosts = 0;
+$nextQuery = "DELETE FROM Posts WHERE post_id='".$delete."'";
+if ($result = $mysqli->query($query))
+{
+  while ($row = $result->fetch_assoc()) 
+  { 
+    $totalPosts +=1;
+    $delete=$row[post_id];
+    $nextQuery = "DELETE FROM Posts WHERE post_id='".$delete."'";
+      if(isset($_POST["post".$totalPosts.""]))
+    {
+      $mysqli->query($nextQuery);
+      echo "Post ID: '".$delete."' was deleted.<br>";
+      }
+  }
+  $result->free();
+}
+$mysqli->close();
 
 //need stuff to get the checkboxes from html page but then
 
