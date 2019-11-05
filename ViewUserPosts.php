c4d8f277f51c019647d465c091b399ceb3936441
Malena Schoeni <mschoeni789@gmail.com>
2019-11-05 09:35:39 -0600

    php finally runs

--- ViewUserPosts.php
+++ ViewUserPosts.php
@@ -1,7 +1,5 @@
 <?php
 
-function getUser() 
-{
     $myPass = "seishae4";
     $mysqli = new mysqli("mysql.eecs.ku.edu", "msch179", $myPass, "msch179");
 
@@ -9,38 +7,13 @@ function getUser()
         printf("Connect failed: %s\n", $mysqli->connect_error);
         exit();
     }
-    $data = "SELECT userID FROM Users;";
-    $result = $mysqli->query($data);
-    if ($result)
+
+    if(isset($_POST['submit']))
     {
-        while($row = $result->fetch_assoc())
-        {
-            echo "<option value=" . $row['userID'] . ">"  . $row['userID'] . "</option>";
-        }
+      $users = $_POST["user"];
     }
-    else
-    {
-        echo "No user found";
-    }
-    $mysqli->close();
-}
-
-
-
-
-    $myPass = "seishae4";
-    $mysqli = new mysqli("mysql.eecs.ku.edu", "msch179", $myPass, "msch179");
-
-    if ($mysqli->connect_errno) {
-        printf("Connect failed: %s\n", $mysqli->connect_error);
-        exit();
-    }
-    $users = $_POST["user"];
     $data = "SELECT * FROM Posts WHERE author_id='$users';";
     $result = $mysqli->query($data);
-    echo "Showing table of posts made by " . $users . "<br>";
-    echo "<table style='border: 1px'>";
-    echo "<tr><td>PostID</td><td><td>Posts</td></tr>";
     if ($result)
     {
         while($row = $result->fetch_assoc())
@@ -59,5 +32,5 @@ function getUser()
 
     $mysqli->close();
 
-    
+
 ?>
